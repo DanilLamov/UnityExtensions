7e43baaa3fd0eae56c6fc80a7c132be1b6e8e1f6
Danil Lamov <Danil_lamov@mail.ru>
2023-01-16 22:17:33 +0300

    added container

---
+++ Assets/Lamov/UnityExtensions/Runtime/Container.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lamov.UnityExtensions.Runtime
+{
+    public class Container
+    {
+        private Dictionary<Type, object> _components;
+
+        public Container()
+        {
+            _components = new Dictionary<Type, object>();
+        }
+        
+        public Container(params object[] components)
+        {
+            _components = new Dictionary<Type, object>();
+            foreach (var component in components)
+            {
+                Add(component);
+            }
+        }
+
+        public void Add<T>(T component) where T : class => _components.Add(component.GetType(), component);
+        
+        public T Get<T>() where T : class => _components[typeof(T)] as T;
+
+        public bool TryGetComponent<T>(out T component) where T : class
+        {
+            if (_components.TryGetValue(typeof(T), out var componentObj))
+            {
+                component = componentObj as T;
+                return true;
+            }
+
+            component = null;
+            return false;
+        }
+    }
+}
---
+++ Assets/Lamov/UnityExtensions/Test/TestContainer.cs
@@ -0,0 +1,23 @@
+using Lamov.UnityExtensions.Runtime;
+using UnityEngine;
+
+namespace Lamov.UnityExtensions.Test
+{
+    public class TestContainer : MonoBehaviour
+    {
+        private void Awake()
+        {
+            var container = new Container(new A(), new B());
+        }
+    }
+
+    public class A
+    {
+        
+    }
+
+    public class B
+    {
+        
+    }
+}
