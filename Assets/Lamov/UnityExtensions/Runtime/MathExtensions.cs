4b059f89c35ad4524c975efc7dfbccca09dcf4b6
Danil Lamov <Danil_lamov@mail.ru>
2023-01-18 17:42:23 +0300

    added math extensions

---
+++ Assets/Lamov/UnityExtensions/Runtime/MathExtensions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Lamov.UnityExtensions.Runtime
+{
+    public static class MathExtensions
+    {
+        public static Vector3 MoveToward(Vector3 current, Vector3 target, float delta)
+        {
+            if (current.Approximately(target)) return target;
+            
+            var movementDirection = target - current;
+            if (movementDirection.magnitude < delta) return target;
+            
+            return current + movementDirection.normalized * delta;
+        }
+
+        public static bool Approximately(this Vector3 a, Vector3 b) => Mathf.Approximately(a.x, b.x) && Mathf.Approximately(a.y, b.y) && Mathf.Approximately(a.z, b.z);
+    }
+}
