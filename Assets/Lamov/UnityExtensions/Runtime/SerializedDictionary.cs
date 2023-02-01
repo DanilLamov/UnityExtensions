a936852b66b6a41ebf087198d344140358f5024a
Danil Lamov <Danil_lamov@mail.ru>
2023-02-01 19:15:27 +0300

    added serialized dictionary

---
+++ Assets/Lamov/UnityExtensions/Runtime/SerializedDictionary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lamov.UnityExtensions.Runtime
+{
+    [Serializable]
+    public class SerializedDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
+    {
+        [SerializeField, HideInInspector] private List<TKey> _keyData = new List<TKey>();
+        [SerializeField, HideInInspector] private List<TValue> _valueData = new List<TValue>();
+        
+        public void OnBeforeSerialize()
+        {
+            _keyData.Clear();
+            _valueData.Clear();
+
+            foreach (var item in this)
+            {
+                _keyData.Add(item.Key);
+                _valueData.Add(item.Value);
+            }
+        }
+
+        public void OnAfterDeserialize()
+        {
+            Clear();
+            for (var i = 0; i < _keyData.Count && i < _keyData.Count; i++)
+            {
+                this[_keyData[i]] = _valueData[i];
+            }
+        }
+    }
+}
---
+++ Assets/Lamov/UnityExtensions/Test/TestDictionary.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lamov.UnityExtensions.Test
+{
+    public class TestDictionary : MonoBehaviour
+    {
+        [SerializeField] private Dictionary<int, string> _dictionary;
+    }
+}
