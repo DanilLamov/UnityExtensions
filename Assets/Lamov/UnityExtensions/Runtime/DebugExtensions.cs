782de5a4ae1d27484ddf4fce0eecd6ec0f36fc35
Danil Lamov <Danil_lamov@mail.ru>
2023-02-14 23:58:14 +0300

    added CalculateExecutionTime

---
+++ Assets/Lamov/UnityExtensions/Runtime/DebugExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Lamov.UnityExtensions.Runtime
+{
+    public static class DebugExtensions
+    {
+        public static TimeSpan CalculateExecutionTime(this Action action)
+        {
+            var time = new Stopwatch();
+            time.Start();
+
+            action.Invoke();
+            
+            return time.Elapsed;
+        }
+    }
+}
---
+++ Assets/Lamov/UnityExtensionsTests/Test/TestExecutionTime.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Lamov.UnityExtensions.Runtime;
+using UnityEngine;
+
+namespace Lamov.UnityExtensionsTests.Test
+{
+    [ExecuteAlways]
+    public class TestExecutionTime : MonoBehaviour
+    {
+        private void Update()
+        {
+            var time = DebugExtensions.CalculateExecutionTime(TestMethod);
+            Debug.Log($"t: {time.Milliseconds}");
+        }
+
+        private void TestMethod()
+        {
+            var list = new List<int>();
+            for (int i = 0; i < 10000; i++)
+            {
+                list.Add(i);
+            }
+
+            var message = "";
+            for (int i = 0; i < 10000; i++)
+            {
+                message += i;
+            }
+        }
+    }
+}
