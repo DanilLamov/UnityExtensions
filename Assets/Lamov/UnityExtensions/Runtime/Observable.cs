f9af8b4b253096004603af9b362c3fa3765ac6b4
Danil Lamov <Danil_lamov@mail.ru>
2023-03-07 18:33:00 +0300

    added Observable

---
+++ Assets/Lamov/UnityExtensions/Runtime/Observable.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lamov.UnityExtensions.Runtime
+{
+    [Serializable]
+    public class Observable<T>
+    {
+        [SerializeField] private T _value;
+        private HashSet<Action<T>> _observers;
+        
+        public T Value {
+            get => _value;
+            set {
+                if (Equals(_value, value)) return;
+                _value = value;
+                Notify();
+            }
+        }
+        
+        public Observable(T value = default) {
+            _value = value;
+            _observers = new HashSet<Action<T>>();
+        }
+        
+        public void Subscribe(Action<T> observer, bool notifyAfterSubscribe = true) {
+            _observers.Add(observer);
+            
+            if(!notifyAfterSubscribe) return;
+            observer?.Invoke(_value);
+        }
+
+        public void Unsubscribe(Action<T> observer) {
+            _observers.Remove(observer);
+        }
+
+        public void UnsubscribeAll() {
+            _observers.Clear();
+        }
+
+        private void Notify() {
+            foreach (var observer in _observers) {
+                observer?.Invoke(_value);
+            }
+        }
+    }
+}
