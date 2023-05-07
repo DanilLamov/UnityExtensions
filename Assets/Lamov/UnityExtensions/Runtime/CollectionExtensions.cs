42cb24967fff4bc6123835d572c4ef68c475dd64
Danil Lamov <Danil_lamov@mail.ru>
2023-05-08 00:45:05 +0300

    added collection extensions

---
+++ Assets/Lamov/UnityExtensions/Runtime/CollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Lamov.UnityExtensions.Runtime
+{
+    public static class CollectionExtensions
+    {
+        static bool ListEquals<T>(List<T> a, List<T> b) 
+        {
+            if (a == null) return b == null;
+            if (b == null || a.Count != b.Count) return false;
+            for (var i = 0; i < a.Count; i++) if (!Equals(a[i], b[i])) return false;
+            
+            return true;
+        }
+    }
+}
